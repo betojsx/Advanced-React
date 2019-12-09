0943eede9bd47d463517b7fe4c608dbc4303604d
Beto <beto@minify.com.br>
2019-12-09 04:15:17 -0300

    Footer height adjustment

--- sick-fits/frontend/components/Footer.js
+++ sick-fits/frontend/components/Footer.js
@@ -3,49 +3,49 @@ import styled from 'styled-components';
 import Container from './styles/Container';
 
 const StyledFooter = styled.footer`
-	background: transparent;
-	height: 270px;
-	background: url('/static/images/waves.png') no-repeat center;
-	background-size: cover;
-	display: flex;
-	align-items: center;
-	> div {
-		width: 100%;
-		/* margin-bottom: 40px; */
-	}
-	.copyright {
-		color: ${props => props.theme.white };
-		text-align: right;
-		strong {
-			font-size: 18px;
-			margin-bottom: 4px;
-			display: block;
-		}
+  background: transparent;
+  height: 20vw;
+  background: url('/static/images/waves.png') no-repeat center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  > div {
+    width: 100%;
+    /* margin-bottom: 40px; */
+  }
+  .copyright {
+    color: ${props => props.theme.white};
+    text-align: right;
+    strong {
+      font-size: 18px;
+      margin-bottom: 4px;
+      display: block;
+    }
 
-		span {
-			font-weight: 300;
-			font-size: 14px;
-		}
-	}
-`
+    span {
+      font-weight: 300;
+      font-size: 14px;
+    }
+  }
+`;
 const Row = styled.div`
-	display: flex;
-	justify-content: flex-end;
-	align-items: center;
-`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
 const Footer = () => {
-	return (
-		<StyledFooter>
-			<Container>
-				<Row>
-					<div className="copyright">
-						<strong>bShoes is a fake shoes ecommerce</strong>
-						<span>Made by Beto</span>
-					</div>
-				</Row>
-			</Container>
-		</StyledFooter>
-	);
-}
+  return (
+    <StyledFooter>
+      <Container>
+        <Row>
+          <div className="copyright">
+            <strong>bShoes is a fake shoes ecommerce</strong>
+            <span>Made by Beto</span>
+          </div>
+        </Row>
+      </Container>
+    </StyledFooter>
+  );
+};
 
 export default Footer;
