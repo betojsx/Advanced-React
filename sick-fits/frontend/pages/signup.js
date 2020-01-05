c3e0b7bdc1b01f58393939cb85ab1dd629f255b2
Beto <beto@minify.com.br>
2020-01-04 21:25:06 -0300

    add signup form

---
+++ sick-fits/frontend/pages/signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from '../components/Signup';
+import Container from '../components/styles/Container';
+
+const SignupPage = () => {
+  return (
+    <Container>
+      <Signup />
+    </Container>
+  );
+};
+
+export default SignupPage;
